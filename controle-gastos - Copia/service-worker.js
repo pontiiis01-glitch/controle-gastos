b24a75528ab9b45ad89b44f3967eda7ebcdb58be
pontiiis01-glitch <pontiiis01@gmail.com>
2026-05-01 12:05:14 -0300

    Add files via upload

---
+++ controle-gastos - Copia/service-worker.js
@@ -0,0 +1,12 @@
+const CACHE_NAME = 'financeiro-v1';
+const assets = ['index.html', 'style.css', 'manifest.json', 'icone2.0.png'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
